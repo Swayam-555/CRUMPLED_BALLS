3ecbab60d29cbda014b9779f739ebb6f0a56120e
Swayam-555 <79014200+Swayam-555@users.noreply.github.com>
2021-04-07 19:45:37 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,22 @@
+class Paper
+{
+constructor(x,y,r)
+{
+paper_options = {
+    isStatic:false,
+    restitution:0.3,
+    friction:0,
+    density:1.2
+}
+
+        this.x=x;
+		this.y=y;
+		this.r=radius;
+		
+
+}
+display()
+{
+
+}
+}
